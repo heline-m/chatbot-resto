1719e62614dc2e5499207804e8317bbebe3084b9
heline-m <eline.malherbe@ecoles-epsi.net>
2024-05-20 13:11:47 +0200

    fix booking code

--- actions/actions.py
+++ actions/actions.py
@@ -31,7 +31,7 @@ class ValidateCommentForm(FormValidationAction):
     def name(self):
         return "validate_comment_form"
 
-    def validate_number_resa(
+    def validate_booking_code(
              self,
              slot_value: Any,
              dispatcher: CollectingDispatcher,
@@ -39,17 +39,17 @@ class ValidateCommentForm(FormValidationAction):
              domain: Dict[Text, Any]
     ) -> Dict[Text, Any]:
 
-        """Validate `number_resa` value."""
+        """Validate `booking_code` value."""
         # Vérifie si le numéro de réservation est déjà présent dans le slot
 
         print("je suis la")
         number = int(slot_value)
 
         if number > 0:
-            return {"number_resa": slot_value}
+            return {"booking_code": slot_value}
         else:
             dispatcher.utter_message(text=f"Votre numéro de réservation ne peut pas être 0")
-            return {"number_resa": None}
+            return {"booking_code": None}
 
     def validate_comment(
             self,
@@ -82,12 +82,12 @@ class ActionSaveComment(Action):
             cursor = conn.cursor()
 
             # TODO a retirer sert juste à debbuger
-            cursor.execute("SELECT id, date, nber_pers, phone, comment FROM reservation")
+            cursor.execute("SELECT id, booking_code, date, nber_pers, phone, comment FROM reservation")
             rows = cursor.fetchall()
 
             print(rows)
 
-            reservation_id = tracker.get_slot('number_resa')
+            reservation_id = tracker.get_slot('booking_code')
             print(reservation_id)
             comment = tracker.get_slot('comment')
             print(comment)
@@ -104,7 +104,7 @@ class ActionSaveComment(Action):
             cursor2.execute('''
             UPDATE reservation
             SET comment = ?
-            WHERE id = ?
+            WHERE booking_code = ?
             ''', (comment, reservation_id))
 
             # Sauvegarder les changements
--- create_database.py
+++ create_database.py
@@ -11,6 +11,7 @@ def create_database():
     # Create table
     cursor.execute('''CREATE TABLE IF NOT EXISTS reservation (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    booking_code INTEGER UNIQUE, 
     name TEXT, 
     date TEXT, 
     nber_pers INTEGER, 
@@ -20,11 +21,11 @@ def create_database():
 
     # Insert example data
     reservation = [
-        ('Robert', '2 Juillet', 2, "06.30.20.52.70", "Je suis intolérant au lactose"),
-        ('Madeleine', '14 Juillet', 3, "06.30.26.52.85", ""),
-        ('Michel', '2 Juin', 4, "06.75.26.98.41", "")
+        (1235, 'Robert', '2 Juillet', 2, "06.30.20.52.70", "Je suis intolérant au lactose"),
+        (1000, 'Madeleine', '14 Juillet', 3, "06.30.26.52.85", ""),
+        (2000, 'Michel', '2 Juin', 4, "06.75.26.98.41", "")
     ]
-    cursor.executemany('INSERT INTO reservation (name, date, nber_pers, phone, comment) VALUES (?,?,?,?,?)', reservation)
+    cursor.executemany('INSERT INTO reservation (booking_code, name, date, nber_pers, phone, comment) VALUES (?,?,?,?,?,?)', reservation)
 
     # Save (commit) the changes
     conn.commit()
